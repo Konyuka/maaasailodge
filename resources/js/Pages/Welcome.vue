58288382645d69fe7529b9642a04bf3e7da027dc
Konyuka <michaelsaiba84@gmail.com>
2023-08-15 22:22:57 +0300

    heroc section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,12 +3,14 @@ import Heroto from '@/Layouts/Heroto.vue';
 import { Head } from '@inertiajs/vue3';
 
 import Navbar from '@/Components/Heroto/Navbar.vue'
+import Landing from '@/Components/Heroto/Landing.vue'
 
 </script>
 
 <template>
     <Heroto title="Maasai-Lodge">
         <Navbar />
+        <Landing />
     </Heroto>
 </template>
 
