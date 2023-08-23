fb91054a34d6860b1c76cc348cc1f8115e4927d9
Konyuka <michaelsaiba84@gmail.com>
2023-08-23 09:24:22 +0300

    animation landing page done

--- resources/js/Components/Heroto/Footer.vue
+++ resources/js/Components/Heroto/Footer.vue
@@ -37,11 +37,10 @@
                                 <a href="#"><img src="img/log.png" alt=""></a>
                             </div>
                             <ul>
-                                <h4>+254 723 160 888</h4>
-                                <h4>info@maasailodge.com</h4>
-                                <h4>Magadi Road, Ongata Rongai</h4>
+                                <h4 class="wow animate__lightSpeedInLeft" data-wow-duration="1s">+254 723 160 888</h4>
+                                <h4 class="wow animate__lightSpeedInLeft" data-wow-duration="1.5s">info@maasailodge.com</h4>
+                                <h4 class="wow animate__lightSpeedInLeft" data-wow-duration="2s">Magadi Road, Ongata Rongai</h4>
                             </ul>
-                            
                         </div>
                     </div>
                     <div class="col-lg-3 offset-lg-1 col-md-5 offset-md-1 col-sm-6">
@@ -66,17 +65,17 @@
                     <div class="col-lg-5 col-md-8 col-sm-12">
                         <div class="footer__newslatter">
                             <h4>Subscribe our newlatester</h4>
-                            <form action="#">
+                            <form action="#" class="wow animate__rubberBand" data-wow-duration="1s">
                                 <input type="text" placeholder="Your E-mail Address">
                                 <button type="submit">Subscribe</button>
                             </form>
                             <div class="footer__newslatter__find">
                                 <h5>Find Us:</h5>
                                 <div class="footer__newslatter__find__links">
-                                    <a href="#"><i class="fa fa-tripadvisor"></i></a>
-                                    <a href="#"><i class="fab fa-facebook"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-tripadvisor wow animate__lightSpeedIn" data-wow-duration="1.5s"></i></a>
+                                    <a href="#"><i class="fab fa-facebook wow animate__lightSpeedInRight" data-wow-duration="2s"></i></a>
+                                    <a href="#"><i class="fab fa-twitter wow animate__lightSpeedInRight" data-wow-duration="2.5s"></i></a>
+                                    <a href="#"><i class="fab fa-instagram wow animate__lightSpeedInRight" data-wow-duration="3s"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -91,7 +90,7 @@
                                 <!-- <script>
                                     document.write(new Date().getFullYear());
                                 </script> -->
-                                All rights reserved | Developed by <a href="https://colorlib.com" target="_blank">Michael Saiba</a>
+                                All rights reserved | Developed by <a href="https://colorlib.com" target="_blank" class="wow animate__rubberBand">Michael Saiba</a>
                             </p>
                         </div>
                     </div>
